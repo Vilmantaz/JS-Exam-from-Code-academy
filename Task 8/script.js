1a9caa2453b4772e81df845ae27a318d8899c2a8
Vilmantas Asanavičius <vilmantaz@gmail.com>
2022-05-18 20:23:52 +0300

    task 8 done

--- Task 8/script.js
+++ Task 8/script.js
@@ -6,3 +6,18 @@ subtraction(a, b) - priima du skaičius ir grąžina jų skirtumą.
 multiplication(a, b) - priima du skaičius ir grąžina jų daugybos rezultatą;
 division(a, b) - priima du skaičius ir grąžina jų dalybos rezultatą;
 ------------------------------------------------------------------------------------ */
+
+function Calculator (a, b) {
+    this.a = a;
+    this.b = b;
+
+    this.sum = () => this.a + this.b;
+    this.subtraction = () => this.a - this.b;
+    this.multiplication = () => this.a * this.b;
+    this.division = () => this.a / this.b;
+}
+const result = new Calculator(50, 5);
+console.log(result.sum());
+console.log(result.subtraction());
+console.log(result.multiplication());
+console.log(result.division());
